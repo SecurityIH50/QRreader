a0a1c8e1df8b043d94d1511c05f8273398e3cefa
SecurityIH50 <138783530+SecurityIH50@users.noreply.github.com>
2023-07-08 21:26:55 +0900

    Add files via upload

--- script/storage.js
+++ script/storage.js
@@ -26,7 +26,7 @@ window.addEventListener('load', (event) => {
     }
     localst_data.reverse();
     for (let index = 0; index < localst_data.length; index++) {
-        htmlinput_text = htmlinput_text + localst_data[index] + "\n";
+        htmlinput_text =+ localst_data[index] + "\n";
     }
     document.getElementById("text_input_to").innerText = htmlinput_text;
 });
