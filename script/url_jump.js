2e86ae0e1017241674704853e1da62f3c4db7ac8
isecurity-creator <138783530+isecurity-creator@users.noreply.github.com>
2024-01-24 22:36:58 +0900

    Add files via upload

--- script/url_jump.js
+++ script/url_jump.js
@@ -7,6 +7,7 @@ function copy_url(dom_id){
     let copy_url_to = dom_id.value;
     try {
         navigator.clipboard.writeText(copy_url_to);
+        console.log(copy_url_to);
         alert("クリップボードにコピーしました。");
     } catch (error) {
         alert("申し訳ありませんが、コピー中に問題が発生しました。\nエラーの詳細：\n\n"+error);
